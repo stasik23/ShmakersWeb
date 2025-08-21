788e587e3580d72295415bef1334a65d9bd4896a
stasik23 <kolesnikkosta572@gmail.com>
2025-08-21 22:05:06 +0300

    adapt fetch for tgbot

--- src/components/ContactComponent/ContactComponent.tsx
+++ src/components/ContactComponent/ContactComponent.tsx
@@ -50,7 +50,7 @@ export const ContactComponent = () => {
         setErrorMessage('');
 
         try {
-            const response = await fetch('https://telegram-bot-topaz-one.vercel.app/api/process-form', {
+            const response = await fetch("https://telegram-bot-topaz-one.vercel.app/api/process-form", {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data),
