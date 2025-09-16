edd723146772665860d74b403ac5bd018629b0a3
stasik23 <kolesnikkosta572@gmail.com>
2025-09-16 22:00:09 +0300

    fix all styles conflicts, project ready

--- src/components/Directions/DirectionsComp.tsx
+++ src/components/Directions/DirectionsComp.tsx
@@ -73,7 +73,6 @@ export const DirectionsComp = () => {
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true, amount: 0.3 }}
                 transition={{ duration: 1.0, ease: [0.22, 1, 0.36, 1] }}
-                style={{ opacity: 0 }}
             >
                 НАПРЯМКИ ДЛЯ РОЗВИТКУ
             </motion.h2>
